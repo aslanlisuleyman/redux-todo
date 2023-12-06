1fb901b6873bc476df2af506eec2b9e230d44d3b
aslanlisuleyman <tu7fzl01d.code.edu.az>
2023-12-07 00:42:33 +0400

    aer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Todo from './Todo'
 import TodoList from './TodoList'
-import AddForm from './addForm'
+import AddForm from './AddForm'
 import Search from './Search'
 import Footer from './Footer'
 
